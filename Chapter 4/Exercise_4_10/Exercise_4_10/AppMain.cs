376faeb909583e90864e128a88892f3003eb48f5
leandro.lima <leandro.lima@engenharia.ufjf.br>
2020-06-20 14:12:12 -0300

    Exercise done

---
+++ Chapter 4/Exercise_4_10/Exercise_4_10/AppMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_4_10
+{
+    public class AppMain
+    {
+        static void Main(string[] args)
+        {
+            GradeBook grade1 = new GradeBook("Math", "Simone");
+
+            grade1.DisplayMessage();
+        }
+    }
+}
---
+++ Chapter 4/Exercise_4_10/Exercise_4_10/GradeBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise_4_10
+{
+    public class GradeBook
+    {
+        private string courseName; // Course name
+
+        public string CourseInstructor { get; set; } //Auto implemented Property to get the instructor's name
+
+        public string CourseName // Property to acces the privte variable
+        {
+            get 
+            {
+                return courseName;
+            }
+            set
+            {
+                courseName = value;
+            }
+        }
+
+        public GradeBook (string initCourseName, string initInstName) // Class' constructor
+        {
+            courseName = initCourseName;
+            CourseInstructor = initInstName;
+        }
+
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to GradeBook of the class: " + courseName);
+            Console.WriteLine("This course is presented by : " + CourseInstructor);
+        }
+    }
+}
